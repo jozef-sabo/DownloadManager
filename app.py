446bdb268a377e03d8db64ae0d8e9250145ccfd2
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2022-06-22 01:25:20 +0200

    Implemented socketio and eventlet to server

--- app.py
+++ app.py
@@ -1,6 +1,49 @@
-from flask import Flask
+import json
+
+from flask import Flask, render_template, request, Response
+from flask_socketio import SocketIO, send, emit
+import eventlet
+# from db import db
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "SecretKeyForFlaskApplicationMadeByJefinko"
+app.config['DATABASE'] = "db/flask_db"
+socketio = SocketIO(app)
+
+
+def bg_emit():
+    socketio.emit(
+        'downloading',
+        {
+            "files_version": 0,
+            "files": [
+                {
+                    "speed": "350k",
+                    "downloaded": "105M"
+                },
+                {
+                    "speed": "15M",
+                    "downloaded": "1263M"
+                }
+            ]
+        }
+    )
+
+
+def listen():
+    while True:
+        bg_emit()
+        eventlet.sleep(10)
+
+
+eventlet.spawn(listen)
+
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
 
 
 @app.route('/')
@@ -41,4 +84,4 @@ def download():
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
