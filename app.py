ed4fb4e7b45bebf0a4b3434562b04bb3a1e2d44f
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2022-06-22 01:24:30 +0200

    Added requests to server

--- app.py
+++ app.py
@@ -4,8 +4,40 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Works!'
+def index():
+    return render_template("index.html")
+
+
+@app.route("/init", methods=["GET"])
+def initialize():
+    response = Response(json.dumps(
+        {
+            "files_version": 0,
+            "files": [
+                {
+                    "title": "Filename.mp4",
+                    "total": "1955M",
+                    "downloaded": "350M",
+                    "speed": "105M"
+                },
+                {
+                    "title": "Filename2.mp4",
+                    "total": "2555M",
+                    "downloaded": "2336M",
+                    "speed": "105M"
+                }
+            ]
+        }
+    ))
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route('/download', methods=["POST"])
+def download():
+    request.json["aaa"] = "abcab"
+
+    return request.json
 
 
 if __name__ == '__main__':
